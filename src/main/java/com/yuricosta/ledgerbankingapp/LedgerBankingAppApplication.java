9f8a102dec4763592b290bd5e319183c3878fb98
Yuri-Costa09 <yurircostawork@gmail.com>
2025-11-22 14:46:47 -0300

    initial files + initial flyway configuration

---
+++ src/main/java/com/yuricosta/ledgerbankingapp/LedgerBankingAppApplication.java
@@ -0,0 +1,13 @@
+package com.yuricosta.ledgerbankingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedgerBankingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LedgerBankingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuricosta/ledgerbankingapp/LedgerBankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuricosta.ledgerbankingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerBankingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
